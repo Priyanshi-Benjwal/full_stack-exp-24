dac34a86828b22d9b5fa14ab139ccf13103afac0
Priyanshi-Benjwal <154530073+Priyanshi-Benjwal@users.noreply.github.com>
2025-11-05 21:00:11 +0530

    Create users.js

---
+++ src/config/users.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    username: "admin",
+    password: "admin123",
+    role: "Admin"
+  },
+  {
+    username: "moderator",
+    password: "mod123",
+    role: "Moderator"
+  },
+  {
+    username: "user",
+    password: "user123",
+    role: "User"
+  }
+];
